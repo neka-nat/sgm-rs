07c3611f0c0fb12cd246b50c69f56d4aeed29b41
nekanat <nekanat.stock@gmail.com>
2021-08-21 13:03:55 +0900

    gh-pages

---
+++ search-index.js
@@ -0,0 +1,4 @@
+var searchIndex = JSON.parse('{\
+"sgm_rs":{"doc":"","t":[0,5],"n":["sgm","compute_disp"],"q":["sgm_rs","sgm_rs::sgm"],"d":["",""],"i":[0,0],"f":[null,[[["usize",15],["grayimage",6]],["grayimage",6]]],"p":[]}\
+}');
+initSearch(searchIndex);
---
+++ source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["sgm_rs"] = {"name":"","files":["lib.rs","sgm.rs"]};
+createSourceSidebar();
